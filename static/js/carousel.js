b49161fbac43b4a3822c9efea373df9736a23a8d
Jacob <jacobholm89@gmail.com>
2014-09-21 10:13:56 +0200

    safe copying

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,1 +1,18 @@
-$(".carousel").
+/*$('#myCarousel').carousel({
+    interval: 10
+});*/
+
+$('.carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  }
+  else {
+      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
