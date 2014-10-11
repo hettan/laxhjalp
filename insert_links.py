ef9daed29ac3a1a3bc191cdd643513b09996f989
Jacob <jacobholm89@gmail.com>
2014-10-11 21:54:46 +0200

    added

--- insert_links.py
+++ insert_links.py
@@ -1,3 +1,4 @@
+from profile import Profile
 from mongo_database_handler import MongoDatabaseHandler  
 db = MongoDatabaseHandler()
 
@@ -5,3 +6,6 @@ db.clear_all()
 db.setup_pages()
 db.add_carousel_links()
 db.add_rut_info_page()
+
+profile = Profile("admin@admin.se", "admin", "admin", "admin")
+db.add_profile(profile)
